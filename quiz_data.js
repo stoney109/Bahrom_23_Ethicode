fb3751adeb771a9f408ec025eb28a5fcb38f91c4
stoney109 <stoney1og@gmail.com>
2025-05-08 15:36:01 +0900

    컨테이너 디자인 일부 수정, 퀴즈 디자인 수정 밑 레이아웃 수정, 퀴즈 형태 수정

---
+++ quiz_data.js
@@ -0,0 +1,167 @@
+const quizData = [
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q1.png",
+    answer: "O",
+    name: "홍길동",
+    birth: "1995.02.13",
+    age: "30세",
+    origin: "서울 / 한국",
+    heightWeight: "176cm / 67kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수1",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수2",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수3",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수4",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수5",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수6",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수7",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수8",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수9",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수10",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수11",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수12",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수13",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  },
+  {
+    question: "과연 사진 속의 인물은 범인일까요?",
+    img: "images/q2.png",
+    answer: "X",
+    name: "김민수14",
+    birth: "1992.11.01",
+    age: "33세",
+    origin: "부산 / 한국",
+    heightWeight: "180cm / 72kg",
+    race: "아시아"
+  }
+];
